df3a3dbfe2ee28fccd4060a0f65a84e9f703e03c
BACompton <compton.boyd@gmail.com>
2016-09-17 13:50:36 -0400

    Added a request tracking to sensor interface
    This is to prevent multiple reads of the same sensor data from the
    same requesting source. This could happen whenever the requesting loop
    was using the same sleep time as the internal sensor updater thread.

--- sensor_inf.py
+++ sensor_inf.py
@@ -14,7 +14,6 @@ class _SensorUpdate(threading.Thread):
         threading.Thread.__init__(self)
         self._sensor = sensor
         self._sensor_sema = semaphor
-        print self._sensor.get_robot().read_buttons()
 
     def run(self):
         robot = self._sensor.get_robot()
@@ -22,21 +21,19 @@ class _SensorUpdate(threading.Thread):
         while not self._stop:
             self._sensor_sema.acquire()
 
+            self._sensor.request_sources.clear()
             self._sensor.btn_prev = self._sensor.btn_down.copy()
             self._sensor.btn_down.clear()
 
             btns = robot.read_buttons()
 
-
-            print btns
             for btn, value in btns.iteritems():
                 if value:
-                    print btn
                     self._sensor.btn_down[btn] = value
 
             self._sensor_sema.release()
 
-            time.sleep(1+robot_inf.SENSOR_UPDATE_WAIT)
+            time.sleep(robot_inf.SENSOR_UPDATE_WAIT)
 
 
     def stop(self):
@@ -49,6 +46,7 @@ class Sensor:
 
     btn_prev = {}
     btn_down = {}
+    request_sources = {}
 
     def __init__(self, robot):
         self._robot = robot
@@ -68,11 +66,34 @@ class Sensor:
     def get_robot(self):
         return self._robot
 
-    def is_btn_pressed(self, btn):
+    def is_btn_pressed(self, btn, src = "Default", override=False):
+        enc_function = "BtnPress"+str(btn)
         rtn = False
 
         self._sensor_sema.acquire()
-        rtn = not self.btn_prev.has_key(btn) and self.btn_down.has_key(btn)
+        self._add_function_key(enc_function)
+
+        if not self.request_sources[enc_function].has_key(src):
+            rtn = not self.btn_prev.has_key(btn) and self.btn_down.has_key(btn)
+            self.request_sources[enc_function][src] = True
         self._sensor_sema.release()
 
         return rtn
+
+    def is_btn_released(self, btn, src="Default", override=False):
+        enc_function = "BtnRelease"+str(btn)
+        rtn  = False
+
+        self._sensor_sema.acquire()
+        self._add_function_key(enc_function)
+
+        if not self.request_sources[enc_function].has_key(src):
+            rtn = self.btn_prev.has_key(btn) and not self.btn_down.has_key(btn)
+            self.request_sources[enc_function][src] = True
+        self._sensor_sema.release()
+
+        return rtn
+
+    def _add_function_key(self, enc_function):
+        if not self.request_sources.has_key(enc_function):
+            self.request_sources[enc_function] = {}
